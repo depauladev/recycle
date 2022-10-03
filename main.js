7c4d47ffa3040ad06adcf7ab00bd9ec8f2949585
Luiz de Paula <holiveira@k2partnering.com>
2022-10-03 10:52:06 -0300

    add img and achievement

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const cookieEl = document.querySelector('.cookie')
 const scoreEl = document.querySelector('.score')
+const achievementEl = document.querySelector('.achievement')
 
 let score = 0
 
@@ -8,4 +9,10 @@ scoreEl.innerHTML = score
 cookieEl.addEventListener('click', () => {
     score++
     scoreEl.innerHTML = score
+
+    if (score == 10) {
+        achievementEl.innerHTML = 'Parabéns você reciclou 10 itens'
+    }
 })
+
+
